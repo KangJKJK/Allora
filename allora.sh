ac92a8b31641b2b4cd6b487f0e84a19be99035f9
kangjk <96231886+KangJKJK@users.noreply.github.com>
2024-09-19 00:26:03 +0900

    Update allora.sh

--- allora.sh
+++ allora.sh
@@ -9,171 +9,59 @@ GREEN="\033[0;32m"
 RED="\033[0;31m"
 RESET="\033[0m"
 
-# 함수: 명령어 실행 및 결과 확인, 오류 발생 시 사용자에게 계속 진행할지 묻기
-execute_with_prompt() {
-    local message="$1"
-    local command="$2"
-    
-    echo -e "${DARK_YELLOW}${message}${RESET}"
-
-    # 명령어 실행 및 오류 내용 캡처
-    output=$(eval "$command" 2>&1)
-    exit_code=$?
-
-    # 출력 결과를 화면에 표시
-    if [ -n "$output" ]; then
-        echo "$output"
-    fi
-
-    if [ $exit_code -ne 0 ]; then
-        echo -e "${RED}Error: Command failed: $command${RESET}" >&2
-        echo -e "${RED}Detailed Error Message:${RESET}"
-        echo "$output" | sed 's/^/  /'  # 상세 오류 메시지를 들여쓰기하여 출력
-        echo
-
-        # 사용자에게 계속 진행할지 묻기
-        read -p "오류가 발생했습니다. 계속 진행하시겠습니까? (Y/N): " response
-        if [[ ! "$response" =~ ^[Yy]$ ]]; then
-            echo -e "${RED}스크립트를 종료합니다.${RESET}"
-            exit 1
-        fi
-    else
-        echo -e "${GREEN}Success: Command completed successfully.${RESET}"
-    fi
-}
-
-#!/bin/bash
-
-# 최적화 스크립트
-
-echo -e "${GREEN}시스템 최적화 작업을 시작합니다.${NC}"
-
-# 불필요한 패키지 자동 제거
-echo -e "${GREEN}불필요한 패키지 자동 제거 중...${NC}"
-sudo apt autoremove -y
-
-# .deb 파일 삭제
-echo -e "${GREEN}.deb 파일 삭제 중...${NC}"
-sudo rm /root/*.deb
-
-# 패키지 캐시 정리
-echo -e "${GREEN}패키지 캐시 정리 중...${NC}"
-sudo apt-get clean
-
-# /tmp 디렉토리 비우기
-echo -e "${GREEN}/tmp 디렉토리 비우기 중...${NC}"
-sudo rm -rf /tmp/*
-
-# 사용자 캐시 비우기
-echo -e "${GREEN}사용자 캐시 비우기 중...${NC}"
-rm -rf ~/.cache/*
-
-# .sh 및 .rz 파일 삭제
-echo -e "${GREEN}.sh 및 .rz 파일 삭제 중...${NC}"
-sudo rm -f /root/*.sh /root/*.rz
-
-# Docker가 설치되어 있는지 확인
-if command -v docker >/dev/null 2>&1; then
-    echo -e "${GREEN}Docker가 설치되어 있습니다. Docker 관련 작업을 수행합니다.${NC}"
-
-    # Docker 로그 정리 스크립트 작성
-    echo -e "${GREEN}Docker 로그 정리 스크립트 작성 중...${NC}"
-    echo -e '#!/bin/bash\ndocker ps -q | xargs -I {} docker logs --tail 0 {} > /dev/null' | sudo tee /usr/local/bin/docker-log-cleanup.sh
-    sudo chmod +x /usr/local/bin/docker-log-cleanup.sh
-
-    # Docker 로그 정리 작업을 크론에 추가
-    echo -e "${GREEN}크론 작업 추가 중...${NC}"
-    (crontab -l ; echo '0 0 * * * /usr/local/bin/docker-log-cleanup.sh') | sudo crontab -
-
-    # 중지된 모든 컨테이너 제거
-    echo -e "${GREEN}중지된 모든 컨테이너 제거 중...${NC}"
-    sudo docker container prune -f
-
-    # 사용하지 않는 모든 이미지 제거
-    echo -e "${GREEN}사용하지 않는 모든 이미지 제거 중...${NC}"
-    sudo docker image prune -a -f
-
-    # 사용하지 않는 모든 볼륨 제거
-    echo -e "${GREEN}사용하지 않는 모든 볼륨 제거 중...${NC}"
-    sudo docker volume prune -f
-
-    # 사용하지 않는 모든 데이터 정리
-    echo -e "${GREEN}사용하지 않는 모든 데이터 정리 중...${NC}"
-    sudo docker system prune -a -f
-else
-    echo -e "${RED}Docker가 설치되어 있지 않습니다. Docker 관련 작업을 생략합니다.${NC}"
-fi
-
-echo -e "${GREEN}시스템 최적화 작업이 완료되었습니다.${NC}"
-
 echo -e "${BOLD}${DARK_YELLOW}시스템 의존성 업데이트 중...${RESET}"
-execute_with_prompt "sudo apt update -y && sudo apt upgrade -y"
+sudo apt update -y && sudo apt upgrade -y
 
 echo -e "${BOLD}${DARK_YELLOW}필요한 패키지 설치 중...${RESET}"
-
-# 필수 패키지 설치 (ca-certificates, curl, gnupg, ufw, jq, git)
-execute_with_prompt "sudo apt install -y ca-certificates curl gnupg ufw jq git"
+sudo apt install ca-certificates zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev curl git wget make
+sudo apt install curl git jq build-essential gcc unzip wget lz4 -y
 
 echo -e "${BOLD}${DARK_YELLOW}Docker 설치 중...${RESET}"
 
 # Docker GPG 키 및 저장소 설정
-execute_with_prompt 'sudo install -m 0755 -d /etc/apt/keyrings'
-execute_with_prompt 'curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg'
-execute_with_prompt 'echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null'
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 # Docker 설치
-execute_with_prompt 'sudo apt update && sudo apt install -y docker-ce docker-ce-cli containerd.io'
+sudo apt update && sudo apt install -y docker-ce docker-ce-cli containerd.io
 
 echo -e "${BOLD}${DARK_YELLOW}Docker 서비스 활성화 중...${RESET}"
 
 # Docker 서비스 활성화 및 시작
-execute_with_prompt 'sudo systemctl enable docker'
-execute_with_prompt 'sudo systemctl start docker'
+sudo systemctl enable docker
+sudo systemctl start docker
 
 sleep 2
 echo -e "${BOLD}${DARK_YELLOW}Docker 버전 확인 중...${RESET}"
-execute_with_prompt 'docker version'
+docker version
 
 echo -e "${BOLD}${DARK_YELLOW}Docker Compose 설치 중...${RESET}"
 
 # Docker Compose의 최신 버전 정보를 가져와서 설치
 VER=$(curl -s https://api.github.com/repos/docker/compose/releases/latest | grep tag_name | cut -d '"' -f 4)
-execute_with_prompt 'sudo curl -L "https://github.com/docker/compose/releases/download/'"$VER"'/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose'
-execute_with_prompt 'sudo chmod +x /usr/local/bin/docker-compose'
+sudo curl -L "https://github.com/docker/compose/releases/download/$VER/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
 
 echo -e "${BOLD}${DARK_YELLOW}Docker Compose 버전 확인 중...${RESET}"
-execute_with_prompt 'docker-compose --version'
+docker-compose --version
 
 # Docker 그룹이 없으면 생성하고 현재 사용자를 그룹에 추가
 if ! grep -q '^docker:' /etc/group; then
-    execute_with_prompt 'sudo groupadd docker'
+    sudo groupadd docker
     echo
 fi
 
-execute_with_prompt 'sudo usermod -aG docker $USER'
+sudo usermod -aG docker $USER
 
 echo -e "${BOLD}${DARK_YELLOW}UFW 방화벽 설정 중...${RESET}"
 
-# UFW 설치 및 포트 개방
-execute_with_prompt "UFW 설치 중..." "sudo apt-get install -y ufw"
-read -p "UFW를 설치한 후 계속하려면 Enter를 누르세요..."
-execute_with_prompt "UFW 활성화 중..." "sudo ufw enable"
-execute_with_prompt "필요한 포트 개방 중..." \
-    "sudo ufw allow ssh && \
-     sudo ufw allow 22 && \
-     sudo ufw allow 4001 && \
-     sudo ufw allow 4000/tcp && \
-     sudo ufw allow 8001 && \
-     sudo ufw allow 8001/tcp && \
-     sudo ufw allow status"
-
-sleep 2
 
 echo -e "${BOLD}${UNDERLINE}${DARK_YELLOW}Worker 노드 설치 중...${RESET}"
 # Worker 노드 설치
-execute_with_prompt "Allora-network 리포지토리 클론 중..." "git clone https://github.com/allora-network/basic-coin-prediction-node"
+git clone https://github.com/allora-network/basic-coin-prediction-node
 
-# `cd` 명령어로 디렉토리 변경 후 작업 수행
+# cd 명령어로 디렉토리 변경 후 작업 수행
 echo -e "${BOLD}${UNDERLINE}${DARK_YELLOW}디렉토리 변경 중...${RESET}"
 cd basic-coin-prediction-node || { echo "디렉토리 변경 실패"; exit 1; }
 
@@ -222,27 +110,73 @@ EOF
 echo -e "${BOLD}${DARK_YELLOW}config.json 파일이 성공적으로 생성되었습니다!${RESET}"
 echo
 
+#!/bin/bash
+
+# 색상 설정
+GREEN="\033[0;32m"
+RED="\033[0;31m"
+YELLOW="\033[0;33m"
+NC="\033[0m"  # No Color
+
+# 포트가 사용 중인지 확인하는 함수
+check_port() {
+    if lsof -i :$1 > /dev/null; then
+        return 1  # 포트 사용 중
+    else
+        return 0  # 포트 사용 가능
+    fi
+}
+-----------------------------------------------------------------------------------------------------------------
+# 기본 포트 및 최대 포트 설정
+starting_ports=(4000 8000)
+max_port=65535
+
+# 포트 확인 및 변경
+for starting_port in "${starting_ports[@]}"; do
+    desired_port=$starting_port
+    while [ $desired_port -le $max_port ]; do
+        if check_port $desired_port; then
+            echo -e "${GREEN}사용 가능한 포트를 찾았습니다: $desired_port${NC}"
+            break 2
+        fi
+        desired_port=$((desired_port + 1))
+    done
+done
+
+# 포트가 사용 중인 경우 처리
+if [ $desired_port -gt $max_port ]; then
+    echo -e "${RED}사용 가능한 포트를 찾을 수 없습니다.${NC}"
+    exit 1
+fi
+
+# config.json 파일에서 포트 변경
+sed -i "s/\"llamaedge_port\": \".*\"/\"llamaedge_port\": \"$desired_port\"/" $gaianet_base_dir/config.json
+
+# UFW에서 포트 개방
+echo -e "${YELLOW}UFW에서 포트 $desired_port를 개방합니다...${NC}"
+ufw allow $desired_port/tcp
+-----------------------------------------------------------------------------------------------------------------
+
 # 추가 디렉토리 및 권한 설정
-execute_with_prompt 'mkdir worker-data'
-execute_with_prompt 'chmod +x init.config'
+mkdir worker-data
+chmod +x init.config
 sleep 2
-execute_with_prompt './init.config'
+./init.config
 
 echo
 echo -e "${BOLD}${UNDERLINE}${DARK_YELLOW}Docker 컨테이너 빌드 및 시작 중...${RESET}"
 
 # Docker 컨테이너 빌드 및 시작
-execute_with_prompt 'docker compose build'
-execute_with_prompt 'docker-compose up -d'
+docker compose build
+docker-compose up -d
 echo
 sleep 2
 echo -e "${BOLD}${DARK_YELLOW}실행 중인 Docker 컨테이너 확인 중...${RESET}"
-execute_with_prompt 'docker ps'
+docker ps
 echo
-execute_with_prompt 'docker logs -f worker'
+docker logs -f worker
 echo
 
 echo -e "${YELLOW}모든 작업이 완료되었습니다. 컨트롤+A+D로 스크린을 종료해주세요${RESET}"
 echo -e "${BOLD}${RED}다음 링크에서 지갑에 Faucet을 요청하세요: https://faucet.testnet.allora.network/${RESET}"
 echo -e "${BOLD}${UNDERLINE}${CYAN}스크립트 작성자: https://t.me/kjkresearch${RESET}"
-
