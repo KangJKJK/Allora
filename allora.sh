9193330b9c83dd6b0a4473a92dee9bb7c5bebd0c
kangjk <96231886+KangJKJK@users.noreply.github.com>
2024-10-07 12:34:44 +0900

    Update allora.sh

--- allora.sh
+++ allora.sh
@@ -23,11 +23,10 @@ source ~/.bashrc
 
 # Docker 설치
 echo -e "${BOLD}${UNDERLINE}${DARK_YELLOW}Docker 설치 중...${RESET}"
-sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt update
-sudo apt install -y docker-ce docker-ce-cli containerd.io
+sudo apt install -y docker.io
+sudo systemctl start docker
+sudo systemctl enable docker
 sudo usermod -aG docker $USER
 
 # Docker Compose 설치
